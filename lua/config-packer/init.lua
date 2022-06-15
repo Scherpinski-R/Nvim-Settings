e4d29c8623575934e9e628ec3755e6fb9b940116
Scherpinski-R <rsbrandao@inf.ufrgs.br>
2022-06-15 19:57:56 -0300

    added atocomplete for command mode

--- lua/config-packer/init.lua
+++ lua/config-packer/init.lua
@@ -1,15 +1,15 @@
 return require('packer').startup(function()
-  
+
   --packer
   use 'wbthomason/packer.nvim'
 
   -- IDE (treesitter, LSP, vim-cmp)
-  
+
   use{
     'nvim-treesitter/nvim-treesitter',
     run = ':TSUpdate'
   }
-  
+
   use {
     "williamboman/nvim-lsp-installer",
     "neovim/nvim-lspconfig"
@@ -18,6 +18,12 @@ return require('packer').startup(function()
   use {
     'hrsh7th/nvim-cmp',
     'hrsh7th/cmp-nvim-lsp',
+
+	  -- Completion for Command Mode
+    'hrsh7th/cmp-buffer',
+	  'hrsh7th/cmp-path',
+	  'hrsh7th/cmp-cmdline',
+
     'saadparwaiz1/cmp_luasnip',
     'L3MON4D3/LuaSnip'
   }
